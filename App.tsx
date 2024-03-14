3aae1f5c5eae216eb9c6ea7e85d6c8b779fe9603
Abhay Raizada <toabhayraizada@gmail.com>
2024-03-14 22:43:15 +0530

    Make it work

--- App.tsx
+++ App.tsx
@@ -5,12 +5,13 @@
  * @format
  */
 
-import React, {useEffect, useState} from 'react';
-import {SafeAreaView, ScrollView, StatusBar, StyleSheet} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet } from 'react-native';
 
-import {getFormTemplate} from './formstr/formstr';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {PrescriptionCreator} from './components/PrescriptionCreator';
+import { getFormTemplate } from './formstr/formstr';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { PrescriptionCreator } from './components/PrescriptionCreator';
+import 'react-native-url-polyfill/auto';
 
 function App(): React.JSX.Element {
   const backgroundStyle = {
@@ -22,14 +23,16 @@ function App(): React.JSX.Element {
   useEffect(() => {
     console.log('inside useeffect');
     const fetchForm = async () => {
-      console.log('fetchiiiing forrmmm!!!');
-      let form = await getFormTemplate(
-        'eb3df1f89653475f0bcbd22da35f8d2f126db8a68a88a7abedc53535c76c39b4',
-      );
-      setForm(form);
+      if (!form) {
+        console.log('fetchiiiing forrmmm!!!');
+        let form = await getFormTemplate(
+          'eb3df1f89653475f0bcbd22da35f8d2f126db8a68a88a7abedc53535c76c39b4',
+        )
+        setForm(form);
+      }
     };
     fetchForm();
-  }, [form, setForm]);
+  }, [form]);
 
   return (
     <SafeAreaView style={backgroundStyle}>
--- formstr/formstr.ts
+++ formstr/formstr.ts
@@ -1,58 +1,33 @@
 import {SimplePool} from 'nostr-tools';
 import {V1FormSpec} from '@formstr/sdk/dist/interfaces';
 
-const defaultRelays = ['wss://relay.primal.net/', 'wss://relay.hllo.live'];
+const relayList = ['wss://relay.primal.net/', 'wss://relay.hllo.live/'];
 
 export const getFormTemplate = async (formId: string): Promise<{}> => {
   console.log('inside getFormTemplate');
   const pool = new SimplePool();
   let formIdPubkey = formId;
-  let relayList = defaultRelays;
   console.log('everything initialised');
   const filter = {
     kinds: [0],
     authors: [formIdPubkey], //formId is the npub of the form
   };
-  console.log('getting kind0', filter);
-  pool
-    .get(relayList, filter, {maxWait: 10})
-    .then(
-      value => {
-        console.log('got kind0', value);
-        return;
-      },
-      reason => {
-        console.log('failed to get because', reason);
-        return;
-      },
-    )
-    .catch(reason => {
-      console.log('Errored', reason);
-      return;
-    });
-  console.log('after promise');
-
+  let kind0 = null
   try {
-    console.log('inside try');
-    console.log(
-      'Event',
-      await pool.querySync(relayList, filter, {maxWait: 10}),
-    );
-    // console.log('fetching...', fetch('https://github.com'));
-    console.log('Main thread is stuck');
+    console.log('inside trydasdsad');
+    kind0 = await pool.get(relayList, filter)
+    console.log('Main thread is working, got event', kind0);
   } catch (e) {
     console.log('inside catch');
     console.log('error is', e);
   }
 
-  console.log('After everything');
-
   pool.close(relayList);
   let formTemplate;
-  // if (kind0) {
-  //   formTemplate = JSON.parse(kind0.content);
-  // } else {
-  //   throw Error('Form template not found');
-  // }
-  return {};
+  if (kind0) {
+    formTemplate = JSON.parse(kind0.content);
+  } else {
+    throw Error('Form template not found');
+  }
+  return formTemplate;
 };
